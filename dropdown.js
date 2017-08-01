d8dda2ad2018cb132e0431f8aaa9941b16804876
Diwakar Bhatt <diwakar.bhatt@cloudwords.com>
2017-08-01 10:32:52 +0530

    Finished dropdown

--- dropdown.js
+++ dropdown.js
@@ -1,25 +1,24 @@
 let page;
 $(document).ready(function() {
     page = 1;
-    getMovies(page);
+    getNews();
 });
 
-function getMovies(page) {
+function getNews(source) {
+    let newsSource = (source) ? source : "google-news";
     $.ajax({
 
-        url: 'https://cors-anywhere.herokuapp.com/https://newsapi.org/v1/articles?source=buzzfeed&api_key=eceda4a298bb4f7dae1d5c7efd007211',
+        url: `https://cors-anywhere.herokuapp.com/https://newsapi.org/v1/articles?source=${newsSource}&api_key=eceda4a298bb4f7dae1d5c7efd007211`,
         type: 'GET',
         headers: {
             'Accept': 'text/json',
             'Access-Control-Allow-Origin': '*',
             'x-api-key': 'eceda4a298bb4f7dae1d5c7efd007211'
-            // 'Access-Control-Allow-Credentials': 'true',
-            // 'Access-Control-Allow-Methods': 'GET',
-            // 'Access-Control-Request-Headers': 'Accept,Access-Control-Allow-Origin,Access-Control-Allow-Methods'
         },
         crossDomain: true,
         success: function(resp) {
             let articles = resp.articles;
+            $('.content').text("");
             articles.forEach(function(news) {
                 addNewsToUi(news);
             });
@@ -33,6 +32,7 @@ function addNewsToUi(news) {
     let newsTitle = news.title;
     let newsDescription = news.description;
     let newsAuthor = news.author;
-    let newsPanel = '<div class="moviePanel"><div class="moviePosterDiv"><img height="300" width="200" src="' + newsPoster + '" class="moviePoster"></div><div class="movieContent"><h1 class="movieTitle">' + newsTitle + '</h1><p class="movieDescription">' + newsDescription + '</p><p class="movieDescription">' + newsAuthor + '</p></div></div>';
+    let newsUrl = news.url;
+    let newsPanel = '<div class="moviePanel"><div class="moviePosterDiv"><img height="300" width="200" src="' + newsPoster + '" class="moviePoster"></div><div class="movieContent"><h1 class="movieTitle">' + newsTitle + '</h1><p class="movieDescription">' + newsDescription + '</p><p class="movieDescription">' + newsAuthor + '</p><a class="movieDescription" href=' + newsUrl + ' target="_blank">Read More</a></div></div>';
     $(".content").append(newsPanel);
 }
